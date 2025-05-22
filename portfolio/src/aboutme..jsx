a047c017a3e2cf3f5d1a311938cf71bf3929d885
“bisolaf” <adedoyin-folarin@brown.edu>
2025-05-21 20:12:36 -0400

    web

---
+++ portfolio/src/aboutme..jsx
@@ -0,0 +1,15 @@
+function About() {
+    return (
+      <div>
+        <h1>About Me</h1>
+        <p>Resume!</p>
+        <h2>Education</h2>
+        <h2>Brown University</h2>
+        <p>Computer Science (Security)</p>
+        <p>International and Public Affairs (Security)</p>
+        <h3> United World College Dilijan, Armenia</h3>
+      </div>
+    );
+  }
+  
+  export default About;
